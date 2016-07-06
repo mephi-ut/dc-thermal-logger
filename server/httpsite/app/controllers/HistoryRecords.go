e8880ccd162c8291e31e90bd853d187b3feba79f
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2016-07-06 15:20:21 +0300

    Added mobile version

--- server/httpsite/app/controllers/HistoryRecords.go
+++ server/httpsite/app/controllers/HistoryRecords.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	//"fmt"
+	"strconv"
 	"devel.mephi.ru/dyokunev/dc-thermal-logger/server/httpsite/app/models"
 	//"devel.mephi.ru/dyokunev/dc-thermal-logger/server/httpsite/app"
 	"github.com/revel/revel"
@@ -14,14 +15,32 @@ type HistoryRecords struct {
 func (c HistoryRecords) Find(historyRecord models.HistoryRecordFilter, order string, limit int) revel.Result {
 	revel.INFO.Printf("%v", historyRecord)
 
-	historyRecords,err := models.HistoryRecord.Order(order).Limit(limit).Select(historyRecord)
+	scope := models.HistoryRecord.Order(order)
+
+	if (historyRecord.SensorId == 0) {
+		scope  = scope.Where("sensor_id IN (1,2,3)")
+		limit *= 3
+	}
+
+	historyRecords,err := scope.Limit(limit).Select(historyRecord)
 	if err != nil {
 		revel.ERROR.Printf("%v", err.Error())
 	}
 	for i,_ := range historyRecords {
 		historyRecords[i].ConvertedValue -= 273.15
 	}
-	c.RenderArgs["historyRecords"] = historyRecords
+
+	if (historyRecord.SensorId == 0) {
+		historyRecordsBySensor := [4][]models.HistoryRecordFilter{}	// TODO: "Filter" shouldn't be used as just the original type
+		for _,r := range historyRecords {
+			historyRecordsBySensor[ r.SensorId ] = append(historyRecordsBySensor[ r.SensorId ], models.HistoryRecordFilter(r))
+		}
+		for i:=1; i<=3; i++ {
+			c.RenderArgs["historyRecords"+strconv.Itoa(i)] = historyRecordsBySensor[i]
+		}
+	} else {
+		c.RenderArgs["historyRecords"] = historyRecords
+	}
 
 	return c.RenderJson(c.RenderArgs)
 }
